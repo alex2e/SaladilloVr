9daeaa0b19804411d028e3fc2f2dc7403d5e3b28
Laptop\Alejandro <a.santillana@aol.com>
2018-02-14 13:47:38 +0100

    Creación Sphera y Script para que cambie de color al ser mirada (No Funciona)

---
+++ SaladilloVr/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,52 @@
+﻿///////////////////////////////
+// Alejandro Santillana Ruiz
+// Curso 2017-2018
+// SphereScript.cs
+///////////////////////////////
+
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+	/// <summary>
+	/// Material de la esfera cuando no está siendo mirada
+	/// </summary>
+	public Material sphereMaterial;
+	/// <summary>
+	/// Material de la esfera cuando está siendo mirada
+	/// </summary>
+	public Material sphereHoverMaterial;
+	
+
+	/// <summary>
+	/// Cuando se inicia el Script.
+	/// </summary>
+	/// <remarks>
+	/// Se le da a la Sphere el material de inicio.
+	/// </remarks>
+	void Start ()
+	{
+		GetComponent<Renderer>().material = sphereMaterial;	
+	}
+	/// <summary>
+	/// Cuando se empieza a mirar la esfera.
+	/// </summary>
+	/// <remarks>
+	/// Cambio el material de la esfera.
+	/// </remarks>
+	public void HoveredSphere()
+	{
+		GetComponent<Renderer>().material = sphereHoverMaterial;
+	}
+	
+	/// <summary>
+	/// Cuando se deja de mirar la esfera.
+	/// </summary>
+	/// <remarks>
+	/// Cambio el material de la esfera de nuevo.
+	/// </remarks>
+	public void NotHoveredSphere()
+	{
+		GetComponent<Renderer>().material = sphereMaterial;
+	}
+}
