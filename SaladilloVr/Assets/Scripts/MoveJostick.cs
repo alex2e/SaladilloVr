803fceeddc76536c3087b57ec6e6f047aa6477f1
Laptop\Alejandro <a.santillana@aol.com>
2018-02-19 10:10:32 +0100

    Movimiento a traves de una habitación de dos formas (Mirando un boton para movernos y a traves de las teclas)

---
+++ SaladilloVr/Assets/Scripts/MoveJostick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveJostick : MonoBehaviour {
+	
+	/// <summary>
+	/// Máxima velocidad de desplazamiento.
+	/// </summary>
+	public float maxSpeed = 0.5f;
+	
+	/// <summary>
+	/// Fuerza de empuje.
+	/// </summary>
+	public float pushForce = 10f;
+	
+	/// <summary>
+	/// Referencia al Rigibody que queremos mover.
+	/// </summary>
+	public Rigidbody rigibody;
+
+	
+	void Awake()
+	{
+		rigibody = GetComponent<Rigidbody>();
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		//Recuperamos el valor de los ejes horizontal y vertical, son valores normalizados que van de cero a uno.
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis(("Vertical"));
+		
+		//Calculamos el vector de movimiento con la dirección con la que mira la cámara
+		Vector3 moveDirection = (h * Camera.main.transform.right + v * Camera.main.transform.forward).normalized;
+
+		if (rigibody.velocity.magnitude < maxSpeed)
+		{
+			rigibody.AddForce(moveDirection * pushForce);
+		}
+	}
+}
---
+++ SaladilloVr/Assets/Scripts/MovementDisc.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+public class MovementDisc : MonoBehaviour
+{
+	/// <summary>
+	/// Máxima velocidad de desplazamiento.
+	/// </summary>
+	public float maxSpeed = 0.5f;
+	
+	/// <summary>
+	/// Fuerza de empuje.
+	/// </summary>
+	public float pushForce = 10f;
+	
+	/// <summary>
+	/// Nos indica si esta siendo mirada.
+	/// </summary>
+	[HideInInspector]
+	public bool isHover;
+	
+	/// <summary>
+	/// Referencia al Rigibody que queremos mover.
+	/// </summary>
+	public Rigidbody rigibody;
+	
+	// Use this for initializations
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (isHover)
+		{
+			if (rigibody.velocity.magnitude < maxSpeed)
+			{
+				rigibody.AddForce((GvrPointerInputModule.Pointer.CurrentRaycastResult.worldPosition - transform.position).normalized * pushForce);
+			}
+		}
+		
+	}
+	/// <summary>
+	/// Acciones a realizar para el eventeo de mirara el disco.
+	/// </summary>
+	public void StartLookingAtDisc()
+	{
+		isHover = true;
+	}
+	/// <summary>
+	/// Acciones a realizar para el evento de dejar de mirar el disco.
+	/// </summary>
+	public void StopLookingAtDisc()
+	{
+		isHover = false;
+	}
+}
